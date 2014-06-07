0e9bd0f76f332d377959c5bebb56f3d8da5d6501
basseljaafar <basseljaafar@gmail.com>
2014-06-07 13:22:58 -0500

    Step-4-street-fighter

--- js/app.js
+++ js/app.js
@@ -10,7 +10,37 @@ $(this).find(".ryu-ready").show();
 
 $(this).find(".ryu-ready").hide();
 $(this).find(".ryu-still").show();
+})
 
+.mousedown(function(){
+
+console.log("mousedown");
+playHadouken();
+$(this).find(".ryu-ready").hide();
+$(this).find(".ryu-throwing").show();
+//$(".handouken").css({"left":"-195px;"});
+//$(".handouken").show();
+$(".handouken").show().finish().animate(
+{"left":"200px"}, 500, function(){
+$(this).hide();
+$(".handouken").css({"left":"-195px;"});
 });
+
+})
+
+.mouseup(function(){
+
+console.log("mouseup");
+$(this).find(".ryu-throwing").hide();
+$(this).find(".ryu-ready").show();
+//$(".handouken").hide();
+//$(".handouken").css({"left":"-195px"});
+});
+
+function playHadouken () {
+  $("#hadouken-sound")[0].volume = 0.5;
+  $("#hadouken-sound")[0].load();
+  $("#hadouken-sound")[0].play();
+}
 
 });
